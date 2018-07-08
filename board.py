80d7bf24fa7ccce228c259941b1fdb37bb32300f
brian <brian.lause@gmail.com>
2018-07-08 00:09:29 -0500

    For Encapsulation - Move Decisions into Players

--- board.py
+++ board.py
@@ -1,41 +1,41 @@
 import random
 
+verbose = False
+
 
 class Player:
-    def __init__(self):
-        self.num_stones_to_play = 9
+    def __init__(self, name, type, number):
+        self.name = name
+        self.type = type
+        self.number = number
 
+    def is_human(self):
+        return self.type == 'human'
 
-def is_human(player):
-    return False
+    def get_move(self, valid_moves):
+        if self.is_human():
+            return self.get_move_human(valid_moves)
+        else:
+            return self.get_move_computer(valid_moves)
 
-
-def get_move(player, valid_moves):
-    if is_human(player):
-        return get_move_human(player, valid_moves)
-    else:
-        return get_move_computer(valid_moves)
-
-
-def get_move_computer(valid_moves):
-    is_move_valid = False
-    while not is_move_valid:
-        move = random.randint(0, 23)
-        if move in valid_moves:
-            is_move_valid = True
-    return move
-
-
-def get_move_human(player, valid_moves):
-    is_move_valid = False
-    while not is_move_valid:
-        print('Player', player, 'please enter location to MOVE to:')
-        user_input = input()
-        if len(user_input) > 0:
-            move = ord(user_input[0]) - ord('A')
-            if 0 <= move <= 23 and move in valid_moves:
+    def get_move_computer(self, valid_moves):
+        is_move_valid = False
+        while not is_move_valid:
+            move = random.randint(0, 23)
+            if move in valid_moves:
                 is_move_valid = True
-    return move
+        return move
+
+    def get_move_human(self, player, valid_moves):
+        is_move_valid = False
+        while not is_move_valid:
+            print('Player', player, 'please enter location to MOVE to:')
+            user_input = input()
+            if len(user_input) > 0:
+                move = ord(user_input[0]) - ord('A')
+                if 0 <= move <= 23 and move in valid_moves:
+                    is_move_valid = True
+        return move
 
 
 class NineMenGame:
@@ -57,6 +57,17 @@ class NineMenGame:
             (5, 13, 20),
             (2, 14, 23)]
 
+    def __init__(self, p1, p2):
+        self.board = [0 for _ in range(8 * 3)]
+        self.player_to_move = 0
+        self.players = (p1, p2)
+        self.num_stones_to_play = [9, 9]
+
+    def get_state_num(self):
+        board_state = sum([x**3 * y for x, y in enumerate(self.board)])
+        stones_state = sum([x**10 * y for x, y in enumerate(self.num_stones_to_play)])
+        return board_state, stones_state, self.player_to_move
+
     def find_row_and_col(self, pos):
         row = ([any([y == pos for y in x]) for x in self.rows]).index(True)
         col = ([any([y == pos for y in x]) for x in self.cols]).index(True)
@@ -66,19 +77,13 @@ class NineMenGame:
         return self.board[list_of_3[0]] == self.board[list_of_3[1]] and \
                self.board[list_of_3[0]] == self.board[list_of_3[2]]
 
-    def __init__(self):
-        self.board = [0 for _ in range(ord('A'), ord('A') + 8 * 3)]
-        self.player_to_move = 0
-        self.players = (Player(), Player())
-        print('board init')
-
     def display_board(self):
         print('')
         print('It is', self.current_player(), 'turn to play:')
-        letter_board = [chr(x[0] + ord('A')) if x[1] == 0 else x[1] for x in enumerate(self.board)]
+        # letter_board = [chr(x[0] + ord('A')) if x[1] == 0 else x[1] for x in enumerate(self.board)]
         letter_board = ['.' if x[1] == 0 else x[1] for x in enumerate(self.board)]
-        print('Stones to place, player 1:', self.players[0].num_stones_to_play,
-              'player 2:', self.players[1].num_stones_to_play)
+        print('Stones to place, player 1:', self.num_stones_to_play[0],
+              'player 2:', self.num_stones_to_play[1])
         print(letter_board[0], '---', letter_board[1], '---', letter_board[2])
         print('|', letter_board[3], '-', letter_board[4], '-', letter_board[5], '|')
         print('| |', letter_board[6], letter_board[7], letter_board[8], '| |')
@@ -95,8 +100,8 @@ class NineMenGame:
         return int((self.player_to_move ^ 1) + 1)
 
     def capture_piece(self):
-        move = get_move(self.current_player(), self.get_stones(self.current_opponent()))
-        print('====PLAYER', self.current_player(), 'captures piece', move, '!====')
+        move = self.players[self.current_player() - 1].get_move(self.get_stones(self.current_opponent()))
+        # print('====PLAYER', self.current_player(), 'captures piece', move, '!====')
         self.board[move] = 0
 
     def new_line_created(self, move):
@@ -104,24 +109,24 @@ class NineMenGame:
         return self.is_triple_match(self.rows[row]) or self.is_triple_match(self.cols[col])
 
     def place_piece(self):
-        move = get_move(self.current_player(), self.get_open_locations())
+        move = self.players[self.current_player() - 1].get_move(self.get_open_locations())
         self.board[move] = self.current_player()
         if self.new_line_created(move):
             self.capture_piece()
-        self.players[self.player_to_move].num_stones_to_play -= 1
-        self.player_to_move = self.player_to_move ^ 1
+        self.num_stones_to_play[self.player_to_move] -= 1
+        self.player_to_move ^= 1
 
     def make_move(self):
         valid_moves = []
         while valid_moves == []:
-            stone = get_move(self.current_player(), self.get_stones(self.current_player()))
+            stone = self.players[self.current_player() - 1].get_move(self.get_stones(self.current_player()))
             valid_moves = self.get_valid_moves([stone])
-        move = get_move(self.current_player(), valid_moves)
+        move = self.players[self.current_player() - 1].get_move(valid_moves)
         self.board[move] = self.current_player()
         self.board[stone] = 0
         if self.new_line_created(move):
             self.capture_piece()
-        print('Player', self.current_player(), 'moved from', stone, 'to', move, '.')
+        # print('Player', self.current_player(), 'moved from', stone, 'to', move, '.')
         self.player_to_move = self.player_to_move ^ 1
 
     def get_stones(self, player):
@@ -164,20 +169,44 @@ class NineMenGame:
         num_moves = len(moves)
         return num_stones < 3 or num_moves == 0
 
-    def play_game(self):
-        while self.players[self.player_to_move].num_stones_to_play > 0:
-            self.display_board()
-            self.place_piece()
-        num_moves = 0
-        while not self.has_lost():
-            self.display_board()
-            self.make_move()
-            num_moves += 1
-        print('Player', self.current_player(), 'has lost :(')
-        print('Player', self.current_opponent(), 'has won!!! (game took', num_moves, 'moves.)')
+def play_game(player1, player2):
+    game = NineMenGame(player1, player2)
+    state_list = []
+    while game.num_stones_to_play[game.player_to_move] > 0:
+        if verbose:
+            game.display_board()
+        game.place_piece()
+        state_list.append(game.get_state_num())
+    while not game.has_lost():
+        if verbose:
+            game.display_board()
+        game.make_move()
+        state_list.append(game.get_state_num())
+    print('Player', game.current_opponent(), 'has won!!! (game took', len(state_list), 'moves.)')
+    winner = game.current_opponent()
+    return winner, state_list
 
 
+def print_to_file(winner, state_list, file):
+    with open(file, 'a') as f:
+        for state in state_list:
+            f.write(str(state) + ' ' +
+                    # str(state.num_stones_to_play) + ' ' +
+                    str(winner) + '\n')
+
+
+file = "output.txt"
 print("Let's Play")
-game1 = NineMenGame()
-game1.play_game()
+prediction = dict()
+p1 = Player('Brian', 'state', 1)
+p2 = Player('Dani', 'state', 2)
+for _ in range(100):
+    winner, state_list = play_game(p1, p2)
+    print_to_file(winner, state_list, file)
+    for state in state_list:
+        if state in prediction:
+            val, count = prediction[state]
+            prediction[state] = (val * count + winner) / (count + 1), count + 1
+        else:
+            prediction[state] = (winner, 1)
 print("All Done")
