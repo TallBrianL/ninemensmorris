d3608accaeefe073668e7ee12c971b8ff0baf687
TallBrian <brian.lause@gmail.com>
2020-06-07 02:29:28 -0500

    clean up
    Return true for end of game if not properly subclassed.  Correct empty lines.

--- game.py
+++ game.py
@@ -9,13 +9,19 @@ class Game:
         self.players = (p1, p2)
 
     def is_game_over(self):
-        return False
+        return True
 
     def get_valid_moves(self):
         return list()
 
     def take_turn(self, selected_move):
-        return True
+        return
+
+    def display_board(self):
+        return
+
+    def get_state_num(self):
+        return
 
     def play_game(self):
         state_list = []
@@ -32,16 +38,14 @@ class Game:
         print('Player', self.current_opponent(), 'has won!!! (game took', len(state_list), 'moves.)')
         winner = self.current_opponent()
         return winner, state_list
-    
-    
+
     def select_move(self, valid_moves):
         player = self.players[self.current_player() - 1]
         if player.is_human():
             return self.get_move_human(type, player, valid_moves)
         else:
             return self.get_move_computer(player, valid_moves)
-    
-    
+
     def get_move_computer(self, player, valid_moves):
         if not player.type == 'learning':
             move_idx = random.randrange(0, len(valid_moves))
@@ -53,8 +57,7 @@ class Game:
                 if move_idx in valid_moves:
                     is_move_valid = True
         return list(valid_moves)[move_idx]
-    
-    
+
     def get_move_human(self, player, valid_moves):
         self.display_board()
         print('Select from the following valid moves:')
