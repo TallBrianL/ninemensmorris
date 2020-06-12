fcd248fe02a511bf968f42a519e2dfffbffd7c08
TallBrian <brian.lause@gmail.com>
2020-06-12 01:43:21 -0500

    remove dead code

--- game.py
+++ game.py
@@ -44,30 +44,3 @@ class Game:
     
         # Game Over
         return self.winner(), state_list
-
-    def get_move_computer(self, player, valid_moves):
-        if not player.type == 'learning':
-            move_idx = random.randrange(0, len(valid_moves))
-        else:
-            state = self.get_state_string()
-            is_move_valid = False
-            while not is_move_valid:
-                move_idx = player.prediction(state)
-                if move_idx in valid_moves:
-                    is_move_valid = True
-        return list(valid_moves)[move_idx]
-
-    def get_move_human(self, player, valid_moves):
-        self.display_board()
-        print('Select from the following valid moves:')
-        for i, x in enumerate(valid_moves):
-            print(i, x)
-        is_move_valid = False
-        while not is_move_valid:
-            print(player.name, 'Please enter location to ' + type + ':')
-            user_input = input()
-            if len(user_input) > 0:
-                move = ord(user_input[0]) - ord('A')
-                if 0 <= move <= 23 and move in valid_moves:
-                    is_move_valid = True
-        return move
